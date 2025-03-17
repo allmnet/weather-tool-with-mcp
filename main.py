48822d96af6f78c3d4821a20aaed130cc4d8efa8
rairai77 <cutenitki@gmail.com>
2025-03-16 21:43:16 -0700

    got a simple server and client

--- main.py
+++ main.py
@@ -1,6 +1,32 @@
-def main():
-    print("Hello from r2!")
+"""MCP Client"""
+
+import asyncio
+from mcp import ClientSession, StdioServerParameters
+from mcp.client.stdio import stdio_client
+
+# Create server parameters for stdio connection
+server_params = StdioServerParameters(
+    command="uv",  # Executable
+    args=["run", "server.py"],  # Optional command line arguments
+)
+
+
+async def run():
+    """
+    Run the client
+    """
+    async with stdio_client(server_params) as (read, write):
+        async with ClientSession(read, write) as session:
+            # Initialize the connection
+            await session.initialize()
+
+            # Call a tool
+            result = await session.call_tool(
+                "get_todays_weather", arguments={"city_name": "london"}
+            )
+
+            print(result)
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(run())
---
+++ server.py
@@ -0,0 +1,29 @@
+"""
+MCP server to get weather data
+"""
+
+from mcp.server.fastmcp import FastMCP
+from dotenv import dotenv_values
+import requests
+
+
+mcp = FastMCP(name="weather", host="127.0.0.1", port=50000, timeout=30)
+env = dotenv_values()
+WEATHER_URL = "https://api.weatherapi.com/v1/current.json?key=WEATHER_KEY&q=CITY_NAME"
+
+
+@mcp.tool()
+def get_todays_weather(city_name: str) -> str:
+    """
+    Get today's weather for `city_name` city
+    """
+    url = WEATHER_URL.replace("WEATHER_KEY", env["WEATHER_KEY"]).replace(
+        "CITY_NAME", city_name
+    )
+    response = requests.get(url, timeout=10)
+    return response.text
+
+
+if __name__ == "__main__":
+    print("Running server...")
+    mcp.run()
